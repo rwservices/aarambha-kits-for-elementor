3eab4b6eab3eeb259fe47c967d99382211ff72f7
rabindratharu <rabindra.tharu.np@gmail.com>
2025-04-21 12:36:55 +0545

    fixes issue for the editor stop working

--- includes/elementor/widgets/base-widget.php
+++ includes/elementor/widgets/base-widget.php
@@ -1,23 +1,23 @@
 <?php
- /**
-  * Aarambha Kits elementor base widget class
-  *
-  * @link       https://aarambhathemes.com/
-  * @since      1.0.0
-  *
-  * @package    Aarambha_Kits
-  * @subpackage Aarambha_Kits/includes/elementor/widgets
-  * @author     Aarambha Themes <aarambhathemes@gmail.com>
-  */
+
+/**
+ * Aarambha Kits elementor base widget class
+ *
+ * @link       https://aarambhathemes.com/
+ * @since      1.0.0
+ *
+ * @package    Aarambha_Kits
+ * @subpackage Aarambha_Kits/includes/elementor/widgets
+ * @author     Aarambha Themes <aarambhathemes@gmail.com>
+ */
 
 use Elementor\Widget_Base;
 use Elementor\Controls_Manager;
 use Elementor\Icons_Manager;
-use Elementor\Core\Schemes\Color;
 use Elementor\Group_Control_Typography;
 
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
 	exit; // If this file is called directly, abort.
 }
 
@@ -26,7 +26,8 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * Class Base_Widget
  */
-abstract class Base_Widget extends Widget_Base {
+abstract class Base_Widget extends Widget_Base
+{
 
 	/**
 	 * Post number.
@@ -40,7 +41,8 @@ abstract class Base_Widget extends Widget_Base {
 	 *
 	 * @access protected
 	 */
-	protected function register_controls() {
+	protected function register_controls()
+	{
 
 		/*----Content Tab----*/
 		$this->__init_base_content_controls();
@@ -48,16 +50,16 @@ abstract class Base_Widget extends Widget_Base {
 
 		/*----Style Tab----*/
 		$this->__init_base_style_controls();
-
 	}
 
-	protected function __init_base_content_controls() {
+	protected function __init_base_content_controls()
+	{
 
 		// Block Heading Tab
 		$this->start_controls_section(
 			'block_heading',
 			array(
-				'label' => esc_html__( 'Block Heading', 'aarambha-kits' ),
+				'label' => esc_html__('Block Heading', 'aarambha-kits'),
 				'tab'   => Controls_Manager::TAB_CONTENT,
 			)
 		);
@@ -67,7 +69,7 @@ abstract class Base_Widget extends Widget_Base {
 			'widget_title',
 			array(
 				'type'        => Controls_Manager::TEXT,
-				'label'       => esc_html__( 'Text', 'aarambha-kits' ),
+				'label'       => esc_html__('Text', 'aarambha-kits'),
 				'label_block' => true,
 			)
 		);
@@ -76,7 +78,7 @@ abstract class Base_Widget extends Widget_Base {
 		$this->add_control(
 			'widget_title_icon',
 			array(
-				'label'       => esc_html__( 'Icon', 'aarambha-kits' ),
+				'label'       => esc_html__('Icon', 'aarambha-kits'),
 				'type'        => Controls_Manager::ICONS,
 				'label_block' => true,
 			)
@@ -86,12 +88,12 @@ abstract class Base_Widget extends Widget_Base {
 		$this->add_control(
 			'widget_title_icon_position',
 			array(
-				'label'       => esc_html__( 'Icon Position', 'aarambha-kits' ),
+				'label'       => esc_html__('Icon Position', 'aarambha-kits'),
 				'type'        => Controls_Manager::SELECT,
 				'default'     => 'flex-row',
 				'options'     => array(
-					'flex-row'         => esc_html__( 'Left', 'aarambha-kits' ),
-					'flex-row-reverse' => esc_html__( 'Right', 'aarambha-kits' ),
+					'flex-row'         => esc_html__('Left', 'aarambha-kits'),
+					'flex-row-reverse' => esc_html__('Right', 'aarambha-kits'),
 				),
 				'condition'   => array(
 					'widget_title_icon[value]!' => '',
@@ -104,7 +106,7 @@ abstract class Base_Widget extends Widget_Base {
 		$this->add_control(
 			'widget_title_sept',
 			array(
-				'label'     => esc_html__( 'Separator', 'aarambha-kits' ),
+				'label'     => esc_html__('Separator', 'aarambha-kits'),
 				'type'      => Controls_Manager::SWITCHER,
 				'default'   => 'yes',
 				'selectors' => array(
@@ -115,13 +117,14 @@ abstract class Base_Widget extends Widget_Base {
 
 		$this->end_controls_section();
 	}
-	protected function __init_base_query_controls() {
+	protected function __init_base_query_controls()
+	{
 
 		// WP Query
 		$this->start_controls_section(
 			'wp_query_heading',
 			array(
-				'label' => esc_html__( 'WP Query', 'aarambha-kits' ),
+				'label' => esc_html__('WP Query', 'aarambha-kits'),
 				'tab'   => Controls_Manager::TAB_CONTENT,
 			)
 		);
@@ -130,9 +133,9 @@ abstract class Base_Widget extends Widget_Base {
 		$this->add_control(
 			'base_tax_category_post_filter',
 			array(
-				'label'       => esc_html__( 'Categories', 'aarambha-kits' ),
+				'label'       => esc_html__('Categories', 'aarambha-kits'),
 				'type'        => Controls_Manager::SELECT2,
-				'options'     => aarambha_kits_get_terms( 'category' ),
+				'options'     => aarambha_kits_get_terms('category'),
 				'default'     => '',
 				'multiple'    => true,
 				'label_block' => true,
@@ -143,9 +146,9 @@ abstract class Base_Widget extends Widget_Base {
 		$this->add_control(
 			'base_tax_post_tag_post_filter',
 			array(
-				'label'       => esc_html__( 'Tags', 'aarambha-kits' ),
+				'label'       => esc_html__('Tags', 'aarambha-kits'),
 				'type'        => Controls_Manager::SELECT2,
-				'options'     => aarambha_kits_get_terms( 'post_tag' ),
+				'options'     => aarambha_kits_get_terms('post_tag'),
 				'default'     => '',
 				'multiple'    => true,
 				'label_block' => true,
@@ -156,9 +159,9 @@ abstract class Base_Widget extends Widget_Base {
 		$this->add_control(
 			'base_exclude_ids',
 			array(
-				'label'       => esc_html__( 'Posts Exclude', 'aarambha-kits' ),
-				'description' => esc_html__( 'To exclude the posts, enter post ids with comma separator. Ex:- 11,12,13...', 'aarambha-kits' ),
-				'placeholder' => esc_html__( 'Ex:- 11,12,13...', 'aarambha-kits' ),
+				'label'       => esc_html__('Posts Exclude', 'aarambha-kits'),
+				'description' => esc_html__('To exclude the posts, enter post ids with comma separator. Ex:- 11,12,13...', 'aarambha-kits'),
+				'placeholder' => esc_html__('Ex:- 11,12,13...', 'aarambha-kits'),
 				'type'        => Controls_Manager::TEXT,
 				'label_block' => true,
 			)
@@ -168,10 +171,10 @@ abstract class Base_Widget extends Widget_Base {
 		$this->add_control(
 			'base_posts_limit',
 			array(
-				'label'       => esc_html__( 'Posts Limit', 'aarambha-kits' ),
+				'label'       => esc_html__('Posts Limit', 'aarambha-kits'),
 				'type'        => Controls_Manager::NUMBER,
 				'min'         => 1,
-				'default'     => absint( $this->post_number ),
+				'default'     => absint($this->post_number),
 				'label_block' => false,
 			)
 		);
@@ -180,7 +183,7 @@ abstract class Base_Widget extends Widget_Base {
 		$this->add_control(
 			'base_posts_offset',
 			array(
-				'label'       => esc_html__( 'Posts Offset', 'aarambha-kits' ),
+				'label'       => esc_html__('Posts Offset', 'aarambha-kits'),
 				'type'        => Controls_Manager::NUMBER,
 				'default'     => 0,
 				'min'         => 0,
@@ -195,20 +198,20 @@ abstract class Base_Widget extends Widget_Base {
 		$this->add_control(
 			'base_orderby',
 			array(
-				'label'       => esc_html__( 'Order By', 'aarambha-kits' ),
+				'label'       => esc_html__('Order By', 'aarambha-kits'),
 				'type'        => Controls_Manager::SELECT,
 				'separator'   => 'before',
 				'default'     => 'date',
 				'options'     => array(
-					'ID'            => esc_html__( 'Post Id', 'aarambha-kits' ),
-					'author'        => esc_html__( 'Post Author', 'aarambha-kits' ),
-					'title'         => esc_html__( 'Title', 'aarambha-kits' ),
-					'date'          => esc_html__( 'Date', 'aarambha-kits' ),
-					'modified'      => esc_html__( 'Last Modified Date', 'aarambha-kits' ),
-					'parent'        => esc_html__( 'Parent Id', 'aarambha-kits' ),
-					'rand'          => esc_html__( 'Random', 'aarambha-kits' ),
-					'comment_count' => esc_html__( 'Comment Count', 'aarambha-kits' ),
-					'menu_order'    => esc_html__( 'Menu Order', 'aarambha-kits' ),
+					'ID'            => esc_html__('Post Id', 'aarambha-kits'),
+					'author'        => esc_html__('Post Author', 'aarambha-kits'),
+					'title'         => esc_html__('Title', 'aarambha-kits'),
+					'date'          => esc_html__('Date', 'aarambha-kits'),
+					'modified'      => esc_html__('Last Modified Date', 'aarambha-kits'),
+					'parent'        => esc_html__('Parent Id', 'aarambha-kits'),
+					'rand'          => esc_html__('Random', 'aarambha-kits'),
+					'comment_count' => esc_html__('Comment Count', 'aarambha-kits'),
+					'menu_order'    => esc_html__('Menu Order', 'aarambha-kits'),
 				),
 				'label_block' => true,
 			)
@@ -218,12 +221,12 @@ abstract class Base_Widget extends Widget_Base {
 		$this->add_control(
 			'base_order',
 			array(
-				'label'       => esc_html__( 'Order', 'aarambha-kits' ),
+				'label'       => esc_html__('Order', 'aarambha-kits'),
 				'type'        => Controls_Manager::SELECT,
 				'default'     => 'desc',
 				'options'     => array(
-					'asc'  => esc_html__( 'Ascending', 'aarambha-kits' ),
-					'desc' => esc_html__( 'Descending', 'aarambha-kits' ),
+					'asc'  => esc_html__('Ascending', 'aarambha-kits'),
+					'desc' => esc_html__('Descending', 'aarambha-kits'),
 				),
 				'label_block' => true,
 			)
@@ -232,17 +235,15 @@ abstract class Base_Widget extends Widget_Base {
 		$this->end_controls_section();
 	}
 
-	protected function __init_base_query_extra_controls() {
+	protected function __init_base_query_extra_controls() {}
 
-	}
-
-	protected function __init_base_style_controls() {
-
+	protected function __init_base_style_controls()
+	{
 		// Block Heading Tab
 		$this->start_controls_section(
 			'block_style_heading',
 			array(
-				'label' => esc_html__( 'Block Heading', 'aarambha-kits' ),
+				'label' => esc_html__('Block Heading', 'aarambha-kits'),
 				'tab'   => Controls_Manager::TAB_STYLE,
 			)
 		);
@@ -251,9 +252,9 @@ abstract class Base_Widget extends Widget_Base {
 		$this->add_responsive_control(
 			'block_heading_padding',
 			array(
-				'label'      => esc_html__( 'Padding', 'aarambha-kits' ),
+				'label'      => esc_html__('Padding', 'aarambha-kits'),
 				'type'       => Controls_Manager::DIMENSIONS,
-				'size_units' => array( 'px', '%', 'em' ),
+				'size_units' => array('px', '%', 'em'),
 				'selectors'  => array(
 					'{{WRAPPER}} .aarambha-kits-block-heading-wrap .widget-heading-elements' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				),
@@ -264,9 +265,9 @@ abstract class Base_Widget extends Widget_Base {
 		$this->add_responsive_control(
 			'block_heading_margin',
 			array(
-				'label'      => esc_html__( 'Margin', 'aarambha-kits' ),
+				'label'      => esc_html__('Margin', 'aarambha-kits'),
 				'type'       => Controls_Manager::DIMENSIONS,
-				'size_units' => array( 'px', '%', 'em' ),
+				'size_units' => array('px', '%', 'em'),
 				'selectors'  => array(
 					'{{WRAPPER}} .aarambha-kits-block-heading-wrap' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				),
@@ -288,35 +289,26 @@ abstract class Base_Widget extends Widget_Base {
 			'block_heading_color',
 			array(
 				'type'      => Controls_Manager::COLOR,
-				'label'     => esc_html__( 'Color', 'aarambha-kits' ),
+				'label'     => esc_html__('Color', 'aarambha-kits'),
 				'default'   => '#ffffff',
-				'scheme'    => array(
-					'type'  => Color::get_type(),
-					'value' => Color::COLOR_1,
-				),
 				'selectors' => array(
 					'{{WRAPPER}} .aarambha-kits-block-heading-wrap .widget-heading-elements >*' => 'color: {{VALUE}}',
 					'{{WRAPPER}} .aarambha-kits-block-heading-wrap .widget-heading-elements svg' => 'fill: {{VALUE}}',
 				),
-
 			)
 		);
+
 		// Heading Background Color
 		$this->add_control(
 			'block_heading_background_color',
 			array(
 				'type'      => Controls_Manager::COLOR,
-				'label'     => esc_html__( 'Background Color', 'aarambha-kits' ),
+				'label'     => esc_html__('Background Color', 'aarambha-kits'),
 				'default'   => '#289dcc',
-				'scheme'    => array(
-					'type'  => Color::get_type(),
-					'value' => Color::COLOR_1,
-				),
 				'selectors' => array(
 					'{{WRAPPER}} .aarambha-kits-block-heading-wrap .widget-heading-elements' => 'background-color: {{VALUE}}',
 					'{{WRAPPER}} .aarambha-kits-block-heading-wrap' => 'border-bottom-color: {{VALUE}}',
 				),
-
 			)
 		);
 
@@ -328,41 +320,43 @@ abstract class Base_Widget extends Widget_Base {
 	 *
 	 * @param string $settings The available widget heading options.
 	 */
-	public function widget_heading( $settings ) {
-		
-		if ( empty( $settings['widget_title'] ) ) {
+	public function widget_heading($settings)
+	{
+
+		if (empty($settings['widget_title'])) {
 			return;
 		}
 
-		$this->add_render_attribute( 'widget_heading_wrap', 'class', 'aarambha-kits-block-heading-wrap' );
-		$this->add_render_attribute( 'widget_heading_inner_wrap', 'class', 'd-inline-flex' );
-		if ( isset( $settings['widget_title_icon'] ) && ! empty( $settings['widget_title_icon']['library'] ) ) {
-			$this->add_render_attribute( 'widget_heading_wrap', 'class', 'icon-active' );
+		$this->add_render_attribute('widget_heading_wrap', 'class', 'aarambha-kits-block-heading-wrap');
+		$this->add_render_attribute('widget_heading_inner_wrap', 'class', 'd-inline-flex');
+		if (isset($settings['widget_title_icon']) && ! empty($settings['widget_title_icon']['library'])) {
+			$this->add_render_attribute('widget_heading_wrap', 'class', 'icon-active');
 			// Icon Position
-			$this->add_render_attribute( 'widget_heading_inner_wrap', 'class', esc_attr( $settings['widget_title_icon_position'] ) );
-			$this->add_render_attribute( 'widget_heading_inner_wrap', 'class', 'align-items-center' );
+			$this->add_render_attribute('widget_heading_inner_wrap', 'class', esc_attr($settings['widget_title_icon_position']));
+			$this->add_render_attribute('widget_heading_inner_wrap', 'class', 'align-items-center');
 		}
-		$this->add_render_attribute( 'widget_heading_inner_wrap', 'class', 'widget-heading-elements' );
+		$this->add_render_attribute('widget_heading_inner_wrap', 'class', 'widget-heading-elements');
 
-		?>
-		<h4 <?php $this->print_render_attribute_string( 'widget_heading_wrap' ); ?>>
-			<div <?php $this->print_render_attribute_string( 'widget_heading_inner_wrap' ); ?>>
-				<?php Icons_Manager::render_icon( map_deep( wp_unslash( $settings['widget_title_icon'] ), 'sanitize_text_field' ), [ 'aria-hidden' => 'true' ] ); ?>
-				<label><?php echo esc_html( $settings['widget_title'] ); ?></label>
+?>
+		<h4 <?php $this->print_render_attribute_string('widget_heading_wrap'); ?>>
+			<div <?php $this->print_render_attribute_string('widget_heading_inner_wrap'); ?>>
+				<?php Icons_Manager::render_icon(map_deep(wp_unslash($settings['widget_title_icon']), 'sanitize_text_field'), ['aria-hidden' => 'true']); ?>
+				<label><?php echo esc_html($settings['widget_title']); ?></label>
 			</div>
 		</h4><!-- .aarambha-kits-block-heading-wrap -->
-		<?php
+	<?php
 	}
 
 	/**
 	 * Displays the post title within the widgets.
 	 */
-	public function the_title() {
-		?>
+	public function the_title()
+	{
+	?>
 		<h3 class="entry-title">
 			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 		</h3>
-		<?php
+	<?php
 	}
 
 	/**
@@ -370,11 +364,12 @@ abstract class Base_Widget extends Widget_Base {
 	 *
 	 * @param string $size The featured image size.
 	 */
-	public function the_post_thumbnail( $size = '' ) {
-		?>
+	public function the_post_thumbnail($size = '')
+	{
+	?>
 		<a href="<?php the_permalink(); ?>" class="aarambha-kits-thumb-link">
-			<?php the_post_thumbnail( $size ); ?>
+			<?php the_post_thumbnail($size); ?>
 		</a>
-		<?php
+<?php
 	}
 }
